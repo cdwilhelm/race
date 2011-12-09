489a15e5336cba499f646cdeae7f7694afb61faf
ride9150 <ride9150@b26bfd80-931c-cecd-eb32-ede25e580705>
2011-12-09 14:30:12 +0000

    update to rails 2.3.11

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,3 +8,23 @@ $jq(function(){
 
 
 
+Ajax.Responders.register({
+  onCreate: function(request) {
+    var csrf_meta_tag = $$('meta[name=csrf-token]')[0];
+    if (csrf_meta_tag) {
+      var header = 'X-CSRF-Token',
+          token = csrf_meta_tag.readAttribute('content');
+      if (!request.options.requestHeaders) {
+        request.options.requestHeaders = {};
+      }
+      request.options.requestHeaders[header] = token;
+    }
+  }
+});
+
+
+/*
+$jq(document).ajaxSend(function(e, xhr, options) {
+  var token = $("meta[name='csrf-token']").attr("content");
+  xhr.setRequestHeader("X-CSRF-Token", token);
+});*/
